0ae8a2ce547bc564d4ce2d15cd92431ffd96d6a6
FULLSTACK <jumin0220@naver.com>
2023-06-08 17:16:39 +0900

    컨트롤러에 @RequestMapping작성하기

--- src/main/java/jiho/spring/mvc/book/boookmarket/controller/BookController.java
+++ src/main/java/jiho/spring/mvc/book/boookmarket/controller/BookController.java
@@ -4,11 +4,11 @@ import jiho.spring.mvc.book.boookmarket.model.Book;
 import jiho.spring.mvc.book.boookmarket.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -16,18 +16,17 @@ import java.util.List;
 
 
 @Controller
-
+@RequestMapping("/books")
 public class BookController {
     @Autowired
     private BookService bookService;
 
-  @RequestMapping(value = "/books",method = RequestMethod.GET)
+    @RequestMapping
     public String requestBookList(Model model){
         List<Book> list=bookService.getAllBookList();
         model.addAttribute("bookList",list);
         return "books";
 
     }
-
 
 }
