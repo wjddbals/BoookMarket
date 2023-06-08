c3877aa4643889be8e324e2f60a9ff5735988fa3
FULLSTACK <jumin0220@naver.com>
2023-06-08 16:51:21 +0900

    BookService 인터페이스 구현

---
+++ src/main/java/jiho/spring/mvc/book/boookmarket/service/BookService.java
@@ -0,0 +1,11 @@
+package jiho.spring.mvc.book.boookmarket.service;
+
+import jiho.spring.mvc.book.boookmarket.model.Book;
+
+import java.util.List;
+
+public interface BookService {
+    List<Book> getAllBookList();
+
+
+}
