9198cc9c2aa6be33afa670eafc00a5772c82ce0a
FULLSTACK <jumin0220@naver.com>
2023-06-08 18:17:43 +0900

    @MatrixVariable에 Map사용-Example06controller

--- src/main/java/jiho/spring/mvc/book/boookmarket/controller/Example06Controller.java
+++ src/main/java/jiho/spring/mvc/book/boookmarket/controller/Example06Controller.java
@@ -21,13 +21,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/home")
 public class Example06Controller {
 
-    @GetMapping("/exam06")
- public String requestMethod(Model model){
-     model.addAttribute("data","ModelMap 예제입니다");
-     model.addAttribute("data2","웹 요청 URL은 /home/exam06입니다");
-     return "webpage05";
+    @GetMapping("/exam06/{bookId}/category/{category}")
+    public String requestMethod(
+            @MatrixVariable MultiValueMap<String,String> matrixVars,
+            @MatrixVariable(pathVar = "category" ) MultiValueMap<String,String> matrixVars2,
+            Model model){
+        System.out.println(matrixVars);
+        System.out.println(matrixVars2);
 
- }
+        model.addAttribute("data",matrixVars +"<br>" + matrixVars2);
+
+        return "webpage06";
+
+    }
 
 
 }
