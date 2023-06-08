0481d9c204d6f6c2a4bc77242f4772c02f1701c3
FULLSTACK <jumin0220@naver.com>
2023-06-08 16:45:03 +0900

    book클래스 만들기

---
+++ src/main/java/jiho/spring/mvc/book/boookmarket/model/Book.java
@@ -0,0 +1,17 @@
+package jiho.spring.mvc.book.boookmarket.model;
+
+public class Book {
+    private String bookId;
+    private String name;
+    private int unitPrice;
+    private String author;
+    private String description;
+    private String publisher;
+    private String category;
+    private long unitInStock;
+    private String releaseDate;
+    private String condition;
+
+
+
+}
