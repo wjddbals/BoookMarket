3e943206ce6b4e002654c813e916ed9512a6aa3d
FULLSTACK <jumin0220@naver.com>
2023-06-09 18:00:55 +0900

    요청 처리 매서드 매개변수에 @ModelAttribute적용

--- src/main/java/jiho/spring/mvc/book/boookmarket/chap07/Example02Controller.java
+++ src/main/java/jiho/spring/mvc/book/boookmarket/chap07/Example02Controller.java
@@ -9,13 +9,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class Example02Controller {
 
 	@PostMapping("/member")
-	public String submitForm(@ModelAttribute Member member, Model model) {		
-		System.out.println("@PostMapping----------------");
-		System.out.println("���̵�: " + member.getId());
-		System.out.println("��й�ȣ: " + member.getPasswd());
-		System.out.println("������ : " + member.getCity());
-		System.out.println("�� ��: " + member.getSex());
-		System.out.print("�� ��: ");
+	public String submitForm(@ModelAttribute Member member, Model model) {
+		System.out.println("@GetMapping----------------");
+		System.out.println("아이디 : " +member.getId());
+		System.out.println("비밀번호; " +member.getPasswd());
+		System.out.println("거주지 :" +member.getCity());
+		System.out.println("성별: " +member.getSex());
+		System.out.println("취미:");
 		for(int i=0;i<member.getHobby().length;i++)
 			System.out.print("["+ member.getHobby()[i]+ "] ");
 		
