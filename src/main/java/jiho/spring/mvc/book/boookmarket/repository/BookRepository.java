a4b47b7d9e675a5cc1a15b3fad5e0a804fa08318
FULLSTACK <jumin0220@naver.com>
2023-06-09 15:24:14 +0900

    BookRepository인터페이스에 getBookListByFilter메서드선언

--- src/main/java/jiho/spring/mvc/book/boookmarket/repository/BookRepository.java
+++ src/main/java/jiho/spring/mvc/book/boookmarket/repository/BookRepository.java
@@ -3,10 +3,12 @@ package jiho.spring.mvc.book.boookmarket.repository;
 import jiho.spring.mvc.book.boookmarket.model.Book;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 
 public interface BookRepository {
     List<Book> getAllBookList();
     List<Book> getBookListByCategory(String category);
-
+    Set<Book> getBookListByFilter(Map<String,List<String>> filter);
 }
