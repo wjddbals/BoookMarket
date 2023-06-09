7c5a384b89fcbc9cda9423e78cf49dae6f9cb3f5
FULLSTACK <jumin0220@naver.com>
2023-06-09 15:54:47 +0900

    BookServiceImpl 클래스에 requestbooksbyfilter메서드 추가

--- src/main/java/jiho/spring/mvc/book/boookmarket/controller/BookController.java
+++ src/main/java/jiho/spring/mvc/book/boookmarket/controller/BookController.java
@@ -6,11 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.MatrixVariable;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 //
 
 
@@ -45,5 +48,12 @@ public class BookController {
         model.addAttribute("bookLIst",booksByCategory);
         return "books";
     }
-
+    @GetMapping("/filter/{bookFilter}")
+    public String requestBooksByFilter(
+            @MatrixVariable(pathVar="bookFilter") Map<String,List<String>> bookFilter,
+            Model model) {
+        Set<Book> booksByFilter = bookService.getBookListByFilter(bookFilter);
+        model.addAttribute("bookList", booksByFilter);
+        return "books";
+    }
 }
