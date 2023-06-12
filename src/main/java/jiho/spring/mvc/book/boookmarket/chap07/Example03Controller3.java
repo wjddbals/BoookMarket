4a351b59afb03169448c4488319b72d60231d90b
FULLSTACK <jumin0220@naver.com>
2023-06-12 12:44:16 +0900

    매서드 수준의 @ModelAttribute적용하기

--- src/main/java/jiho/spring/mvc/book/boookmarket/chap07/Example03Controller3.java
+++ src/main/java/jiho/spring/mvc/book/boookmarket/chap07/Example03Controller3.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
-public class Example03Controller {
+public class Example03Controller3 {
 
 	@GetMapping("/exam03")
 	public String showForm() {
@@ -15,11 +15,11 @@ public class Example03Controller {
 
 	@ModelAttribute("title")
 	public String setTitle() {
-		return "@ModelAttribute ����";
+		return "@ModelAttribute 유형";
 	}
 
 	@ModelAttribute
 	public void setsubTitle(Model model) {
-		model.addAttribute("subtitle", "�޼ҵ忡 @ModelAttribute �ֳ����̼� �����ϱ�");
+		model.addAttribute("subtitle", "메서드에 @ModelAttribute 애노테이션 적용하기");
 	}
 }
---
+++ src/main/webapp/WEB-INF/views/webpage07_03.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: FULLSTACK
+  Date: 2023-06-01
+  Time: 오후 4:59
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html; charset=utf-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<html>
+<head>
+    <title>form Tag</title>
+</head>
+<body>
+<h2>${title}</h2>
+<h4>${subtitle}</h4>
+</body>
+</html>
